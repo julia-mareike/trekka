e3904a32ec7f5d2d0071ea64c4117764afa62d94
julia-mareike <jnmareike@gmail.com>
2020-04-24 18:19:06 +1200

    Setup: create gallery page

---
+++ src/pages/gallery.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+import get from 'lodash/get'
+import Helmet from 'react-helmet'
+import Layout from '../components/layout'
+
+class Gallery extends React.Component {
+  render() {
+    const siteTitle = get(this, 'props.data.site.siteMetadata.title')
+    const [logo] = get(this, 'props.data.allContentfulLogo.edges')
+    const [sideBar] = get(this, 'props.data.allContentfulSidebar.edges')
+    const page = get(this, 'props.data.contentfulPage')
+
+    return (
+      <Layout location={this.props.location} logo={logo.node} sidebar={sideBar.node}>
+        <div style={{ background: '#fff' }} className="sidebar-child">
+          <Helmet title={siteTitle} />
+          <div className="wrapper">
+            <h3 className="section-headline">Gallery</h3>
+            <p>Photos go here</p>
+          </div>
+        </div>
+      </Layout>
+    )
+  }
+}
+
+export default Gallery
+
+export const pageQuery = graphql`
+  query GalleryQuery {
+    allContentfulLogo {
+      edges {
+        node {
+          title
+          image {
+            fixed {
+              ...GatsbyContentfulFixed
+            }
+          }
+        }
+      }
+    }
+    allContentfulSidebar {
+      edges {
+        node {
+          image {
+            fixed(width: 175) {
+              ...GatsbyContentfulFixed
+            }
+          }
+          header
+          content
+        }
+      }
+    }
+  }
+`
