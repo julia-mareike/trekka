42ab9d8f1331069d0dace08ce55334519c0cf1ea
julia-mareike <jnmareike@gmail.com>
2020-04-24 18:37:36 +1200

    Setup: pull trekka-the-book page from contentful

--- src/pages/trekka-the-book.js
+++ src/pages/trekka-the-book.js
@@ -9,24 +9,19 @@ class TrekkaTheBook extends React.Component {
     const siteTitle = get(this, 'props.data.site.siteMetadata.title')
     const [logo] = get(this, 'props.data.allContentfulLogo.edges')
     const [sideBar] = get(this, 'props.data.allContentfulSidebar.edges')
+    const page = get(this, 'props.data.contentfulPage')
 
     return (
       <Layout location={this.props.location} logo={logo.node} sidebar={sideBar.node}>
         <div style={{ background: '#fff' }} className="sidebar-child">
           <Helmet title={siteTitle} />
           <div className="wrapper">
-            <h2 className="section-headline">How to purchase</h2>
-            <p>Copies of “The Trekka Dynasty” can be purchased direct from the author and publisher Todd Niall.</p>
-            <p>The book costs $NZ18 and for international postage rates please just ask.</p>
-            <p>There is no payment method through this website, but Paypal and other options are available.</p>
-            <p>Just drop me an email at trekkaproject@paradise.net.nz and we can sort it out.</p>
-            <p>Also available for purchase via <a href="https://www.trademe.co.nz/Browse/SearchResults.aspx?searchString=trekka+the+book" target="_blank" rel="noopener noreferrer">TradeMe</a>.
-            </p>
-            <br />
-            <p>Todd Niall has produced for Radio New Zealand two documentaries on the Trekka.</p>
-            <p>“The Trekka’s Tale” is in two half hour parts and traces the history of the vehicle based on interviews conducted with those involved with the production.</p>
-            <p>“Trekka Goes to Venice” is a  half hour documentary recorded in Venice at the opening of Michael Stevenson’s exhibition in 2003, which represented New Zealand at the Venice Biennale.</p>
-            <p>For information on how to order and pricing, contact Radio New Zealand’s Replay Radio, at replayradio@radionz.co.nz</p>
+            <h2 className="section-headline">{page.header}</h2>
+            <div className="page-content"
+              dangerouslySetInnerHTML={{
+                __html: page.content.childMarkdownRemark.html,
+              }}
+            />
           </div>
         </div>
       </Layout>
@@ -38,6 +33,14 @@ export default TrekkaTheBook
 
 export const pageQuery = graphql`
   query TrekkaTheBookQuery {
+    contentfulPage(title: {eq: "Trekka the Book"}) {
+      header
+      content {
+        childMarkdownRemark {
+          html
+        }
+      }
+    }
     allContentfulLogo {
       edges {
         node {
